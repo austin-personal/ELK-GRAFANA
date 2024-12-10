5c37fa38aced4fbccc9906a440512473c97a9b56
changmo <kmaestro043@gmail.com>
2024-12-10 16:37:10 +0900

    1. Add logger in app.py(Not working)
    2. Update logstash& filebeat 설정 파일들
    
    3. 그에 따른 도커 컴포즈 수정

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import os
 from flask_cors import CORS
 import time
 import logging # 로그 수집 라이브러리
+from logging.handlers import RotatingFileHandler
 
 # APM 관련 import
 from elasticapm.contrib.flask import ElasticAPM
@@ -11,13 +12,24 @@ from elasticapm.contrib.flask import ElasticAPM
 app = Flask(__name__)
 CORS(app)
 
-# 로그 디렉토리 확인 및 생성(없다면)
+# 로그 디렉토리 확인 및 생성
 log_dir = '/app/logs'
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-# 로그 수집
-logging.basicConfig(filename='/app/logs/app.log', level=logging.INFO,format='%(asctime)s %(levelname)s %(message)s')
+# Flask 로거 설정
+file_handler = RotatingFileHandler(os.path.join(log_dir, 'app.log'), maxBytes=100000, backupCount=10)
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+# 기존의 기본 핸들러 제거 (중복 로그 방지)
+if app.logger.hasHandlers():
+    app.logger.handlers.clear()
+    app.logger.addHandler(file_handler)
+
 
 # APM 설정 (환경변수를 사용하거나 하드코딩 가능)
 app.config['ELASTIC_APM'] = {
@@ -51,6 +63,11 @@ def buy():
     app.logger.info("Received /buy request")
 
     return jsonify({"status": "ok", "data": data}), 200
+
+@app.route('/test-log')
+def test_log():
+    app.logger.info("Test log entry")
+    return "Test log entry created", 200
 
 if __name__ == '__main__':
     # 로컬 테스트 시, app.run() 사용
